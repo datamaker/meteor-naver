fd904cc8aa5d9147bcf3ba0233b3ce4c28d47eb7
권정빈 <datamaker@urbanbase.com>
2017-09-13 15:08:23 +0900

    권정빈 /

--- package.js
+++ package.js
@@ -1,19 +1,19 @@
 Package.describe({
   name: 'datamaker:naver-oauth',
-  version: '0.0.2',
+  version: '0.0.4',
   summary: 'Naver OAuth flow',
   git: 'https://github.com/datamaker/meteor-naver',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.use('oauth2', ['client', 'server']);
-  api.use('oauth', ['client', 'server']);
-  api.use('http', ['server']);
-  api.use('underscore', 'client');
-  api.use('random', 'client');
+  api.use('oauth2@1.1.11', ['client', 'server']);
+  api.use('oauth@1.1.13', ['client', 'server']);
+  api.use('http@1.2.11', ['server']);
+  api.use('underscore@1.0.10', 'client');
+  api.use('random@1.0.10', 'client');
   api.use('peerlibrary:xml2js@0.4.4_3');
-  api.use('service-configuration', ['client', 'server']);
+  api.use('service-configuration@1.0.11', ['client', 'server']);
 
   api.addFiles('naver_server.js', 'server');
   api.addFiles('naver_client.js', 'client');
